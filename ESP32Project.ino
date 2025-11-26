6d5712d8dbd54bb4bdf38516100b97075d5e931b
IREStephen <g00465649@atu.ie>
2025-11-26 23:48:49 +0000

    connected esp32 to wifi

--- ESP32Project.ino
+++ ESP32Project.ino
@@ -2,12 +2,22 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
+const char* ssid = "STEPHE";
+const char* password = "zyqq4921";
 WebServer server(80);
 
 void setup() {
-  Serial.begin(115200);
+ Serial.begin(115200);
+
+ //WiFi 
   WiFi.mode(WIFI_STA);
-  // WiFi credentials will be added later
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
 }
 
 void loop() {
